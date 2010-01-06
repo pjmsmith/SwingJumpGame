4893a0b19e646d82ca59a7b59607d4aafcf18aa0
smittycent <smittycent@09cd1ef4-fa72-11de-91aa-85f76f42a988>
2010-01-06 06:29:12 +0000

    project template ftw

---
+++ SwingJump/Classes/SwingJumpAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  SwingJumpAppDelegate.h
+//  SwingJump
+//
+//  Created by Patrick Smith and Sean Stavropoulos on January 5, 2010.
+//  Copyright LaziestPeopleAlive 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SwingJumpAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ SwingJump/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SwingJump
+//
+//  Created by Patrick Smith and Sean Stavropoulos on January 5, 2010.
+//  Copyright LaziestPeopleAlive 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+	NSAutoreleasePool *pool = [NSAutoreleasePool new];
+	int retVal = UIApplicationMain(argc, argv, nil, @"SwingJumpAppDelegate");
+	[pool release];
+	return retVal;
+}
