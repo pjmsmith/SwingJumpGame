0adf128bc194b12f98c516b1c2179721847e52dc
smittycent <smittycent@09cd1ef4-fa72-11de-91aa-85f76f42a988>
2010-01-07 19:54:30 +0000

    swing chain classes

---
+++ SwingJump/Classes/SwingChain.h
@@ -0,0 +1,16 @@
+//
+//  SwingChain.h
+//  SwingJump
+//
+//  Created by Patrick Smith on 1/7/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface SwingChain : CCNode {
+
+}
+
+@end
---
+++ SwingJump/Classes/SwingChain.m
@@ -0,0 +1,14 @@
+//
+//  SwingChain.m
+//  SwingJump
+//
+//  Created by Patrick Smith on 1/7/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "SwingChain.h"
+
+
+@implementation SwingChain
+
+@end
