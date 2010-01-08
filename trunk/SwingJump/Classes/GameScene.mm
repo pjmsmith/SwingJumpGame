7ebd6729add70eaa83c8f5fa5abcc61f6e60656a
Sean.Stavro <Sean.Stavro@09cd1ef4-fa72-11de-91aa-85f76f42a988>
2010-01-08 23:36:43 +0000

    swing physics

--- trunk/SwingJump/Classes/GameScene.mm
+++ trunk/SwingJump/Classes/GameScene.mm
@@ -10,11 +10,13 @@
 #import "SimpleAudioEngine.h"
 #import "GameScene.h"
 #import "Box2D.h"
+#import "GLES-Render.h"
 
 #define PTM_RATIO 32
 
 b2World *world;
 b2Body* body;
+b2Body *chainBody;
 CCSprite *ball;
 
 @implementation GameScene
@@ -40,11 +42,11 @@ CCSprite *ball;
     if (self != nil) {
         CCSprite *swingSet = [CCSprite spriteWithFile:@"swingset_supports.png"];
         [swingSet setPosition:ccp(240,160)];
-        [self addChild:swingSet];
+        [self addChild:swingSet z:4];
         swingChain = [CCSprite spriteWithFile:@"swingchain.png"];
         [swingChain setAnchorPoint:ccp(0.5,1)];
-        [swingChain setPosition:ccp((swingSet.contentSize.width/2), (swingSet.contentSize.height)-2)];
-        [swingSet addChild:swingChain z:-1];
+        //[swingChain setPosition:ccp((swingSet.contentSize.width/2), (swingSet.contentSize.height)-2)];
+        [self addChild:swingChain z:1];
 		
 		
 		//PHYSICS STARTS HERE
@@ -58,36 +60,102 @@ CCSprite *ball;
 		float borderSize = 96/PTM_RATIO;
 		worldAABB.lowerBound.Set(-borderSize, -borderSize);
 		worldAABB.upperBound.Set(screenSize.width/PTM_RATIO+borderSize, screenSize.height/PTM_RATIO+borderSize);
-		b2Vec2 gravity(0.0f, -30.0f);
+		b2Vec2 gravity(0.0f, -10.0f);
 		bool doSleep = true;
 		world = new b2World(worldAABB, gravity, doSleep);
         
+		world->SetContinuousPhysics(true);
+		
+		
+		GLESDebugDraw *m_debugDraw = new GLESDebugDraw(PTM_RATIO);
+		uint32 flags = 0;
+		flags += 1	* b2DebugDraw::e_shapeBit;
+		//flags += 1	* b2DebugDraw::e_jointBit;
+		//flags += 1	* b2DebugDraw::e_controllerBit;
+		//flags += 1	* b2DebugDraw::e_coreShapeBit;
+		//flags += 1	* b2DebugDraw::e_aabbBit;
+		//flags += 1	* b2DebugDraw::e_obbBit;
+		//flags += 1	* b2DebugDraw::e_pairBit;
+		//flags += 1	* b2DebugDraw::e_centerOfMassBit;
+		m_debugDraw->SetFlags(flags);
+		world->SetDebugDraw(m_debugDraw);
+		
 		//Create a ground box
 		b2BodyDef groundBodyDef;
-		groundBodyDef.position.Set(screenSize.width/PTM_RATIO/2, -1.0f);
+		groundBodyDef.position.Set(screenSize.width/PTM_RATIO/2, 1.3f);
 		b2Body* groundBody = world->CreateBody(&groundBodyDef);
 		b2PolygonDef groundShapeDef;
-		groundShapeDef.SetAsBox(screenSize.width/PTM_RATIO/2, 1.0f);
+		groundShapeDef.SetAsBox(screenSize.width/PTM_RATIO, 1.0f);
 		groundBody->CreateShape(&groundShapeDef);
+		
+		//Create pivot point for swing
+		b2BodyDef pivotBodyDef;
+		pivotBodyDef.position.Set(screenSize.width/PTM_RATIO/2, 247.0f/PTM_RATIO);
+		b2Body* pivotBody = world ->CreateBody(&pivotBodyDef);
+		b2PolygonDef pivotBodyShapeDef;
+		pivotBodyShapeDef.SetAsBox(0.1f,.1f);
+		pivotBody->CreateShape(&pivotBodyShapeDef);
         
+		//Create chain body and shape
+		b2BodyDef chainBodyDef;
+		//chainBodyDef.position.Set(screenSize.width/PTM_RATIO/2, screenSize.height/PTM_RATIO/2);
+		chainBodyDef.position.Set(7.5f,5.5f);
+		//chainBodyDef.userData = swingChain;
+		
+		chainBody = world->CreateBody(&chainBodyDef);
+		b2PolygonDef chainShapeDef;
+		chainShapeDef.SetAsBox(.1f, 2.0f);
+		chainShapeDef.density =  100.0f;
+		chainShapeDef.friction = 0.5f;
+		chainShapeDef.restitution = 0.0f;
+		chainBody->CreateShape(&chainShapeDef);
+		chainBody->SetMassFromShapes();
+		
+		//link
+		//b2RevoluteJointDef chainJoint;
+		//chainJoint.Initialize(&(*pivotBody),&(*chainBody), *(new b2Vec2(1.0f, 0.1f)));
+		//world->CreateJoint(&chainJoint);
+
+		
+		b2RevoluteJointDef rj;
+		rj.Initialize(&(*chainBody), &(*pivotBody), pivotBody->GetPosition());
+		//rj.collideConnected = true;
+		rj.motorSpeed = 0.0f;
+		rj.maxMotorTorque = 100.0f;
+		rj.enableMotor = true;
+		world->CreateJoint(&rj);
+		
+		
 		//Create ball body and shape
 		b2BodyDef ballBodyDef;
-		ballBodyDef.position.Set(150.0f/PTM_RATIO, 400.0f/PTM_RATIO);
-		ballBodyDef.userData = ball;
+		ballBodyDef.position.Set(10.0f/PTM_RATIO, 400.0f/PTM_RATIO);
+		//ballBodyDef.userData = ball;
         
 		body = world->CreateBody(&ballBodyDef);
 		b2CircleDef ballShapeDef;
-		ballShapeDef.radius = 20.0f/PTM_RATIO;
-		ballShapeDef.density = 1.0f;
+		ballShapeDef.radius = 50.0f/PTM_RATIO;
+		ballShapeDef.density = 1000.0f;
 		ballShapeDef.friction = 0.3f;
-		ballShapeDef.restitution = 0.8f;
+		ballShapeDef.restitution = 0.6f;
 		body->CreateShape(&ballShapeDef);
 		body->SetMassFromShapes();
-        
+		
+		b2Vec2 force = b2Vec2(3.0f,0.0f);
+		body->SetLinearVelocity(force);
+		
+		
+		
+		
 		[self schedule:@selector(tick:)];
 		
     }
     return self;
+}
+-(void) draw{
+	[super draw];
+	glEnableClientState(GL_VERTEX_ARRAY);
+	world->DrawDebugData();
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
 
 -(void)tick:(ccTime) dt{
@@ -185,7 +253,8 @@ CCSprite *ball;
     {
         [self.gl.swingChain runAction:[CCSequence actions:[CCRotateBy actionWithDuration:0.1 angle:5],
                                        [CCCallFunc actionWithTarget:self selector:@selector(rotateChainLeft)],nil]];
-    }    
+    chainBody->ApplyTorque(-2000.0f);
+	}    
 }
 
 - (void)rotateChainRight
@@ -194,7 +263,8 @@ CCSprite *ball;
     {
         [self.gl.swingChain runAction:[CCSequence actions:[CCRotateBy actionWithDuration:0.1 angle:-5],
                                        [CCCallFunc actionWithTarget:self selector:@selector(rotateChainRight)],nil]];
-    }
+    chainBody->ApplyTorque(2000.0f);
+	}
 }
 
 @end
