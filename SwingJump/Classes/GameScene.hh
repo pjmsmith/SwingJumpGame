ed55655caa9e6379295fbae966dbcf2bd17199ec
Sean.Stavro <Sean.Stavro@09cd1ef4-fa72-11de-91aa-85f76f42a988>
2010-01-16 06:18:32 +0000

    Swing is less crappy. Will probably need to be rebuilt from the ground up. SUXORZ.

--- SwingJump/Classes/GameScene.hh
+++ SwingJump/Classes/GameScene.hh
@@ -13,8 +13,8 @@
 #import "Biped.h"
 #import "BipedDef.h"
 
-#define numLinks 20
-#define handLink numLinks-3
+#define numLinks 40
+#define handLink numLinks-6
 #define headLinkLength 4.6f
 
 @interface GameScene  : CCScene {}
